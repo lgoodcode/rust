2a9961ee4c2456b284cedeff86fa611b65875e3d
Lawrence Good <lawrence.good55@gmail.com>
2022-08-18 11:18:09 -0700

    tuples - low-power

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -17,23 +17,14 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    return _color.int_value();
+    _color as u32
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    return match value {
-        0 => String::from("Black"),
-        1 => String::from("Brown"),
-        2 => String::from("Red"),
-        3 => String::from("Orange"),
-        4 => String::from("Yellow"),
-        5 => String::from("Green"),
-        6 => String::from("Blue"),
-        7 => String::from("Violet"),
-        8 => String::from("Grey"),
-        9 => String::from("White"),
-        _ => String::from("value out of range"),
-    };
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => format!("{:?}", resistor),
+        Err(_) => "value out of range".to_string(),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
