344d761a272570da9fa8b2c7ac5c3468cc6a4a0c
Lawrence Good <lawrence.good55@gmail.com>
2022-08-18 16:02:20 -0700

    Option - role-playing-games

---
+++ role-playing-game/src/lib.rs
@@ -0,0 +1,45 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health != 0 {
+            return None;
+        }
+
+        let mut player = Player {
+            health: 100,
+            mana: None,
+            level: self.level,
+        };
+
+        if self.level >= 10 {
+            player.mana = Some(100);
+        }
+
+        Some(player)
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        if self.mana.is_none() || self.level < 10 {
+            if self.health < mana_cost {
+                self.health = 0;
+            } else {
+                self.health -= mana_cost;
+            }
+            return 0;
+        } else if self.mana.is_some() && self.mana < Some(mana_cost) {
+            return 0;
+        } else {
+            self.mana = Some(self.mana.unwrap() - mana_cost);
+            return mana_cost * 2;
+        }
+    }
+}
