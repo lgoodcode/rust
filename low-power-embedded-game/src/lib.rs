164ceb9c400e2d9a424098b6697a27d88e7a4ff6
Lawrence Good <lawrence.good55@gmail.com>
2022-08-18 17:58:42 -0700

    tuples - low-power (refactor)

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -7,7 +7,7 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate().filter(|(i, x)| i % 2 == 0).map(|(_, x)| x)
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
